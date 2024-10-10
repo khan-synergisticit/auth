11be0c099f636f2dc626c7c456a8e61e23758f39
american-worx <khan@americanworx.com>
2024-10-10 13:19:22 -0400

    jwt2

--- src/main/java/com/americanworx/auth/controller/LoginController.java
+++ src/main/java/com/americanworx/auth/controller/LoginController.java
@@ -3,9 +3,13 @@ package com.americanworx.auth.controller;
 
 import com.americanworx.auth.config.jwt.JwtProvider;
 import com.americanworx.auth.domain.AuthResponse;
+import com.americanworx.auth.domain.UserDtls;
+import com.americanworx.auth.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,15 +19,25 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @RestController
 public class LoginController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
     private final SecurityContextRepository securityContextRepository =
             new HttpSessionSecurityContextRepository();
 
@@ -59,5 +73,39 @@ public class LoginController {
 
     public record LoginRequest(String username, String password) {
     }
+
+    @PostMapping("/saveUser")
+    public ResponseEntity<?> saveUser(@ModelAttribute UserDtls user, @RequestParam("img") MultipartFile file)
+            throws IOException {
+
+        Boolean existsEmail = userService.existsEmail(user.getEmail());
+
+        if (existsEmail) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
+        } else {
+            String imageName = file.isEmpty() ? "default.jpg" : file.getOriginalFilename();
+            user.setProfileImage(imageName);
+            UserDtls saveUser = userService.saveUser(user);
+
+            if (!ObjectUtils.isEmpty(saveUser)) {
+                if (!file.isEmpty()) {
+                    File saveFile = new ClassPathResource("static/img").getFile();
+
+                    Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "profile_img" + File.separator
+                            + file.getOriginalFilename());
+
+//					System.out.println(path);
+                    Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                }
+                return ResponseEntity.status(HttpStatus.CREATED).body(saveUser);
+
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Something went wrong");
+            }
+        }
+
+
+    }
+
 
 }
