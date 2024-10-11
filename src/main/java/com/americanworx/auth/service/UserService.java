c0eec34063526e82207cd08c16c08d3bc127f6e0
american-worx <khan@americanworx.com>
2024-10-11 07:35:15 -0400

    auth1

--- src/main/java/com/americanworx/auth/service/UserService.java
+++ src/main/java/com/americanworx/auth/service/UserService.java
@@ -12,25 +12,6 @@ public interface UserService {
 
     public List<UserDtls> getUsers(String role);
 
-    public Boolean updateAccountStatus(Integer id, Boolean status);
-
-    public void increaseFailedAttempt(UserDtls user);
-
-    public void userAccountLock(UserDtls user);
-
-    public boolean unlockAccountTimeExpired(UserDtls user);
-
-    public void resetAttempt(int userId);
-
-    public void updateUserResetToken(String email, String resetToken);
-
-    public UserDtls getUserByToken(String token);
-
-    public UserDtls updateUser(UserDtls user);
-
-    public UserDtls updateUserProfile(UserDtls user, MultipartFile img);
-
-    public UserDtls saveAdmin(UserDtls user);
 
     public Boolean existsEmail(String email);
 }
