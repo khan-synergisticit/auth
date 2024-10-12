9286d1edec6ee3b42405301103ef965bda5a417b
american-worx <khan@americanworx.com>
2024-10-12 10:09:35 -0400

    auth

--- src/main/java/com/americanworx/auth/controller/LoginController.java
+++ src/main/java/com/americanworx/auth/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 //import org.springframework.http.MediaType;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -43,8 +44,8 @@ public class LoginController {
         this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping(value = "/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<AuthResponse> login( LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
 
         System.out.println("--------------------------- " + loginRequest.toString());
         Authentication authenticationRequest =
