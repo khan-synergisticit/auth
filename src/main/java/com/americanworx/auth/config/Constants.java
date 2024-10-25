2c2dbe4a2d059ca6e2f7620856b72826a981592f
american-worx <khan@americanworx.com>
2024-10-25 06:50:13 -0400

    url

--- src/main/java/com/americanworx/auth/config/Constants.java
+++ src/main/java/com/americanworx/auth/config/Constants.java
@@ -1,10 +1,7 @@
 package com.americanworx.auth.config;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class Constants {
-    @Value("${url.auth-client}")
-    private static String url;
-    public static final String AUTH_CLIENT_URL = url;
+
+    public static final String AUTH_CLIENT_URL = "http://192.168.1.129";
 
 }
