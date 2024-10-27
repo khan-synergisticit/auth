eabf889b98fb751aff69f0713e3b65dde4d070fc
american-worx <khan@americanworx.com>
2024-10-27 18:11:29 -0400

    url

--- src/main/java/com/americanworx/auth/config/Constants.java
+++ src/main/java/com/americanworx/auth/config/Constants.java
@@ -2,6 +2,6 @@ package com.americanworx.auth.config;
 
 public class Constants {
 
-    public static final String AUTH_CLIENT_URL = "http://192.168.1.76";
+    public static final String AUTH_CLIENT_URL = "http://192.168.1.111";
 
 }
